edb1526f65396e67b6d1247a52399defdad35956
HuyThien <57531802+HuyThien@users.noreply.github.com>
2021-10-06 21:24:12 +0700

    Updates

---
+++ precache-manifest.6cee0938a831c9c3529f733a8fdb8246.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "448d31ade298dc4ad46cd75881ead7ab",
+    "url": "/refactor_18120572/index.html"
+  },
+  {
+    "revision": "07f4749be5911880ab09",
+    "url": "/refactor_18120572/static/css/main.6a39486e.chunk.css"
+  },
+  {
+    "revision": "5dd62328aadfcf03feb2",
+    "url": "/refactor_18120572/static/js/2.d0bb701d.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "/refactor_18120572/static/js/2.d0bb701d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07f4749be5911880ab09",
+    "url": "/refactor_18120572/static/js/main.938dc6b5.chunk.js"
+  },
+  {
+    "revision": "2c6576da9b70c6aed3a6",
+    "url": "/refactor_18120572/static/js/runtime-main.fa528ebb.js"
+  }
+]);
